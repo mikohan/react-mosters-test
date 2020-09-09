ce2c9401fce64ba1f57d0b548752cc2b243a7499
mikohan <angara99@gmail.com>
2020-09-09 18:54:40 +0300

    Updates

---
+++ precache-manifest.21d61bb4b20dccd40035f22c6608d05b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2f64c877f23b13c6b5179962c8cad2",
+    "url": "/react-mosters-test/index.html"
+  },
+  {
+    "revision": "e0c94083af71377d7458",
+    "url": "/react-mosters-test/static/css/main.f2ae9b31.chunk.css"
+  },
+  {
+    "revision": "93362e00098863a8ce90",
+    "url": "/react-mosters-test/static/js/2.7bcc2581.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-mosters-test/static/js/2.7bcc2581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0c94083af71377d7458",
+    "url": "/react-mosters-test/static/js/main.9b3311ae.chunk.js"
+  },
+  {
+    "revision": "a47bf336dd2caf5cdec2",
+    "url": "/react-mosters-test/static/js/runtime-main.eb11b6a9.js"
+  },
+  {
+    "revision": "679bc7e9571df979342267ff4c292313",
+    "url": "/react-mosters-test/static/media/0.679bc7e9.png"
+  },
+  {
+    "revision": "6071d95d5cc7eb9e313e5320d040014e",
+    "url": "/react-mosters-test/static/media/1.6071d95d.png"
+  },
+  {
+    "revision": "eae81ff076f5a55ccd8401e424cb513f",
+    "url": "/react-mosters-test/static/media/2.eae81ff0.png"
+  },
+  {
+    "revision": "2bdfb500c6861e35ceffc7aba5f19407",
+    "url": "/react-mosters-test/static/media/3.2bdfb500.png"
+  },
+  {
+    "revision": "cf92a958c8e2d615b7b41e1550bb1f68",
+    "url": "/react-mosters-test/static/media/4.cf92a958.png"
+  },
+  {
+    "revision": "9d2f3e73c02ac66ea981cb215a3a729b",
+    "url": "/react-mosters-test/static/media/5.9d2f3e73.png"
+  },
+  {
+    "revision": "d7ac8bae6869be92ec05c02cbb1fabf1",
+    "url": "/react-mosters-test/static/media/6.d7ac8bae.png"
+  },
+  {
+    "revision": "863438043f3dd0bb2397896a4f3c7912",
+    "url": "/react-mosters-test/static/media/7.86343804.png"
+  },
+  {
+    "revision": "e607a6f64fcaaf57ceb5349cc59e15aa",
+    "url": "/react-mosters-test/static/media/8.e607a6f6.png"
+  },
+  {
+    "revision": "9cc25c0590d22a539ed92f0062344f8c",
+    "url": "/react-mosters-test/static/media/9.9cc25c05.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.01d812d0f7904c396aae1fd6ab2c18cc.js"
+  "/react-mosters-test/precache-manifest.21d61bb4b20dccd40035f22c6608d05b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-mosters-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

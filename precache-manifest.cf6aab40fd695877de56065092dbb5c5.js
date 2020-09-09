dec1af6255de23fc7a08135cb518ea995baf8ab2
mikohan <angara99@gmail.com>
2020-09-09 18:05:26 +0300

    Updates

---
+++ precache-manifest.cf6aab40fd695877de56065092dbb5c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c04f806c7b724d4566b3c2ede530d151",
+    "url": "/react-mosters-test/index.html"
+  },
+  {
+    "revision": "67bc97d56fa9cc897ac6",
+    "url": "/react-mosters-test/static/css/main.750a9f0b.chunk.css"
+  },
+  {
+    "revision": "93362e00098863a8ce90",
+    "url": "/react-mosters-test/static/js/2.7bcc2581.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-mosters-test/static/js/2.7bcc2581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67bc97d56fa9cc897ac6",
+    "url": "/react-mosters-test/static/js/main.defbd53f.chunk.js"
+  },
+  {
+    "revision": "a47bf336dd2caf5cdec2",
+    "url": "/react-mosters-test/static/js/runtime-main.eb11b6a9.js"
+  }
+]);
